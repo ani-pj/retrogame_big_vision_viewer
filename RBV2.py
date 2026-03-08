a7bfdd0d70dc9fcd6b89add1836cee145e7222ff
ani0184 <e7214@g.maizuru-ct.ac.jp>
2026-03-08 12:34:24 +0900

    処理スピード確保のための改善

--- RBV2.py
+++ RBV2.py
@@ -1,16 +1,24 @@
 import cv2
+import time
+import sys
 
 bilateralFilter_d = 15
 bilateralFilter_sigma_color = 50
 bilateralFilter_sigma_space = 50
 
-def image_proc(data):
-    data = cv2.resize( # 拡大処理
-        data, (data.shape[1]*2, data.shape[0]*2), interpolation=cv2.INTER_LANCZOS4)
+def image_proc(data, RESIZE_INTERPOLATION):
+    # t2 = time.time()
     gaussian = cv2.GaussianBlur(data, (0, 0), 10)
     data = cv2.addWeighted(data, 1.5, gaussian, -0.5, 0)
+    # t3 = time.time()
     data = cv2.bilateralFilter(data, 15, 50, 50) # ノイズ除去
-    #data = cv2.fastNlMeansDenoisingColored(data, None, h=10, hColor=7, templateWindowSize=5, searchWindowSize=10)
+    # t4 = time.time()
+    data = cv2.resize( # 拡大処理
+        data, (data.shape[1]*2, data.shape[0]*2), interpolation=RESIZE_INTERPOLATION)
+    # t5 = time.time()
+    # print("シャープ化処理：", (t3-t2)*1000, "[ms], ノイズ除去：", (t4-t3)*1000, "[ms], 拡大処理：", (t5-t4)*1000, "[ms]")
+
+
     return data
 
 if __name__ == "__main__":
@@ -22,18 +30,46 @@ if __name__ == "__main__":
             if not line:
                 continue
             key, value = line.split()
-            config[key] = int(value)
+            try:
+                config[key] = int(value)
+            except:
+                config[key] = str(value)
+
+    print(config)
+
+    prev_time = 0
+    fps = 0
 
     #カメラの設定 デバイスIDは1
     cap = cv2.VideoCapture(config["DEVICE"], cv2.CAP_DSHOW)
     bilateralFilter_d = config["BILATERAL_FILTER_D"]
     bilateralFilter_sigma_color = config["BILATERAL_FILTER_SIGMA_COLOR"]
     bilateralFilter_sigma_space = config["BILATERAL_FILTER_SIGMA_SPACE"]
+
     COMPARE_MODE = config["COMPARE_MODE"]
-    print(COMPARE_MODE)
+    FPS_VIEW = config["FPS_VIEW"]
+
+    RESIZE_INTERPOLATION = None
+    if config["RESIZE_INTERPOLATION"] == "INTER_NEAREST":
+        RESIZE_INTERPOLATION = cv2.INTER_NEAREST
+    elif config["RESIZE_INTERPOLATION"] == "INTER_LINEAR":
+        RESIZE_INTERPOLATION = cv2.INTER_LINEAR
+    elif config["RESIZE_INTERPOLATION"] == "INTER_CUBIC":
+        RESIZE_INTERPOLATION = cv2.INTER_CUBIC
+    elif config["RESIZE_INTERPOLATION"] == "INTER_AREA":
+        RESIZE_INTERPOLATION = cv2.INTER_AREA
+    elif config["RESIZE_INTERPOLATION"] == "INTER_LANCZOS4":
+        RESIZE_INTERPOLATION = cv2.INTER_LANCZOS4
+    elif config["RESIZE_INTERPOLATION"] == "INTER_LINEAR_EXACT":
+        RESIZE_INTERPOLATION = cv2.INTER_LINEAR_EXACT
+    elif config["RESIZE_INTERPOLATION"] == "INTER_NEAREST_EXACT":
+        RESIZE_INTERPOLATION = cv2.INTER_NEAREST_EXACT
+    else:
+        RESIZE_INTERPOLATION = cv2.INTER_CUBIC
 
     #ループ
     while True:
+        start_time = time.time()
 
         #カメラからの画像取得
         ret, frame = cap.read()
@@ -41,16 +77,27 @@ if __name__ == "__main__":
 
         if COMPARE_MODE:
             frame2 = frame.copy()
-            frame = image_proc(frame)
+            frame = image_proc(frame, RESIZE_INTERPOLATION)
             h, w = frame.shape[:2]
             half_w = w // 2      # 幅の半分（整数値）
 
             frame2 = cv2.resize(frame2, (int(frame2.shape[1] * h / frame2.shape[0]), h))
-            frame3 = cv2.hconcat([frame2[:, :half_w], frame[:, half_w:]])
-            cv2.imshow('RBV2', frame3)
+            display_frame = cv2.hconcat([frame2[:, :half_w], frame[:, half_w:]])
+
         else:
-            frame = image_proc(frame)
-            cv2.imshow('RBV2', frame) 
+            display_frame = image_proc(frame, RESIZE_INTERPOLATION)
+
+        if FPS_VIEW:
+            current_time = time.time()
+            delta_time = current_time - prev_time
+            if delta_time > 0:
+                fps = 1 / delta_time
+            prev_time = current_time
+            # 画面にFPSを描画 (テキスト, 座標, フォント, スケール, 色, 太さ)
+            cv2.putText(display_frame, f"FPS: {fps:.2f}", (10, 30), 
+                cv2.FONT_HERSHEY_SIMPLEX, 1.0, (0, 255, 0), 2)
+
+        cv2.imshow('RBV2', display_frame)
 
         #繰り返し分から抜けるためのif文
         key = cv2.waitKey(1)
